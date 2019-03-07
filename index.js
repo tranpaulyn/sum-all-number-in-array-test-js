0c927e1ace55608afa5f8718a9b551bf9ff03aed
Paulyn <paulyntran@live.ca>
2019-03-07 00:37:55 +0000

    Completed sum of nested array test

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0
+  array.forEach((item) => {
+    if (typeof item === 'number') {
+      sum += item;
+    } else if (item instanceof Array) {
+      sum += sumItems(item);
+    }
+  })
+  return sum;
 }
 
 module.exports = sumItems;
